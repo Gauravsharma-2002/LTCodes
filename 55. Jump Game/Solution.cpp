7a80bfbd6de65e02c5530fe367817d0cd37b2ecf
Gaurav  Sharma <92368075+Gauravsharma-2002@users.noreply.github.com>
2024-06-13 10:19:08 +0530

    C++ Solution
            Submission Statistics:
            Question #: 55
            Language: C++
            Runtime: 53 ms
            Runtime percentile: 26.05
            Memory: 50.9 MB
            Memory percentile: 50.40

---
+++ 55. Jump Game/Solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+       int farthest = 0;
+       int size = nums.size();
+       for(int i=0;i<size;i++){
+        // any moment we are at the index unexplored by any element of array we say we can not go to it so we return false in that case
+        if(i> farthest ){ return false;}
+        // for rest of element we would do the farthest  exploration for each element 
+        farthest = max(farthest, i+ nums[i]);
+       }
+       return true;
+
+
+        
+    }
+};
