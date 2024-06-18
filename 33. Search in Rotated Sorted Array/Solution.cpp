c906dad51126a7e0fec84c36d96e7a86361f4058
Gaurav  Sharma <92368075+Gauravsharma-2002@users.noreply.github.com>
2024-06-18 16:29:30 +0530

    C++ Solution
            Submission Statistics:
            Question #: 33
            Language: C++
            Runtime: 3 ms
            Runtime percentile: 59.40
            Memory: 13.564 MB
            Memory percentile: 37.13

---
+++ 33. Search in Rotated Sorted Array/Solution.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0 , high = nums.size()-1;
+        int mid  = low + (  high - low )/2;
+        while( low <= high){
+            if(nums[mid]==target){
+                return mid ;
+            }
+            // left part sorted
+            else if(nums[mid]>= nums[low]){
+                if(nums[low]<= target && nums[mid]> target){
+                    high = mid -1;
+
+                }else{
+                    low = mid +1;
+                }
+            }
+            // right part sorted
+            else if(nums[mid]<= nums[high] ){
+                if(nums[mid]< target && nums[high]>= target){
+                    low = mid +1;
+                }
+                else {
+                    high = mid -1;
+                }
+            }
+            mid = low + (high-low)/2;
+        }
+        return -1;
+        
+    }
+};
