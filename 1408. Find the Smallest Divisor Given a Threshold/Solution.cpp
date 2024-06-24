1579a8bcf1f13fb83ce310d759fe4e7e28eee3fd
Gaurav  Sharma <92368075+Gauravsharma-2002@users.noreply.github.com>
2024-06-24 23:52:42 +0530

    C++ Solution
            Submission Statistics:
            Question #: 1408
            Language: C++
            Runtime: 45 ms
            Runtime percentile: 6.06
            Memory: 37.304 MB
            Memory percentile: 5.79

---
+++ 1408. Find the Smallest Divisor Given a Threshold/Solution.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findLargest(vector<int>& nums){
+        int ans = INT_MIN;
+        int size = nums.size();
+        for(int i=0;i<size;i++){
+            if(nums[i]>ans) ans = nums[i];
+        }
+        return ans;
+        
+    }
+    int sum(vector<int> nums,int mid){
+        int size = nums.size();
+        int sum = 0;
+        for(int i=0;i<size;i++){
+            sum += ceil((double)nums[i]/(double)mid);
+        }
+        return sum;
+    }
+    int smallestDivisor(vector<int>& nums, int threshold) {
+        int low=1;
+        int high = findLargest(nums);
+        int mid = low + (high-low)/2;
+        while(low<=high){
+
+            if(sum(nums,mid)<=threshold) {
+                high = mid-1;
+            }
+            else{
+                low = mid+1;
+            }
+            mid = low + (high -low)/2;
+        }
+        return low;
+
+        
+    }
+};
