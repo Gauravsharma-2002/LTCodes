930f01c7da4cea25a9b0fe8d68a86fb9cd615759
Gaurav  Sharma <92368075+Gauravsharma-2002@users.noreply.github.com>
2024-07-01 18:42:04 +0530

    C++ Solution
            Submission Statistics:
            Question #: 22
            Language: C++
            Runtime: 7 ms
            Runtime percentile: 23.61
            Memory: 14.924 MB
            Memory percentile: 70.06

--- 22. Generate Parentheses/Solution.cpp
+++ 22. Generate Parentheses/Solution.cpp
@@ -1,34 +1,22 @@
 class Solution {
-    void solution(vector<string>& arr,int open,int close,string op){
+    // you did a mistake of not passing ans array refference that case the empty array in solution 
+    void solution(vector<string>&arr,int open,int close,string s){
         if(open==0 && close==0){
-            arr.push_back(op);
-            return ;
-
+            arr.push_back(s);
+            return;
         }
 
-        //  i have option to add open to output string every time until openCnt >0
-        if(open!=0){
-            string op1 = op;
-            op1.push_back('(');
-            solution(arr,open-1,close,op1);
-        }
+        if(open>0 ) solution(arr,open-1,close,s+'(');
+        if(close>open) solution(arr,open,close-1,s+')');
+        return;
 
-        if(close> open){
-            string op2 = op;
-            op2.push_back(')');
-            solution(arr,open,close-1,op2);
-        }
-        return ;
+        
     }
 public:
-
     vector<string> generateParenthesis(int n) {
-        int open = n;
-        int close = n;
-        vector<string> arr;
-        string otp = "";
-        solution(arr,open,close,otp);
-        return arr;
+        vector<string> ans;
+        solution(ans,n,n,"");
+        return ans;
         
     }
 };
