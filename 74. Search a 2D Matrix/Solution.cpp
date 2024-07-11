bda842c42c2c65ed20e78d205f66415a7fedad84
Gaurav  Sharma <92368075+Gauravsharma-2002@users.noreply.github.com>
2024-07-11 22:44:16 +0530

    C++ Solution
            Submission Statistics:
            Question #: 74
            Language: C++
            Runtime: 0 ms
            Runtime percentile: 100.00
            Memory: 12.148 MB
            Memory percentile: 10.51

---
+++ 74. Search a 2D Matrix/Solution.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int rows = matrix.size();
+        int clm= matrix[0].size();
+        for(int i =0;i<rows;i++){
+            for(int j=0;j<clm;j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
